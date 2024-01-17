6a77fd84a78b00be4c810d67118ef94ac0749939
Vtipol <151755385+Vtipol@users.noreply.github.com>
2024-01-17 14:22:45 +0100

    Update graph.cs

--- algoritmi/graph.cs
+++ algoritmi/graph.cs
@@ -1,13 +1,16 @@
-﻿using System;
+using System;
 namespace Tree
 {
 	public class Graph
 	{
+		List<Alberografico> nodi;
 		int[][] Disegno;
 
 		public Graph(int grandezza)
 		{
-			Disegno = new int[grandezza][];
+
+           nodi = new List<Alberografico>();
+            Disegno = new int[grandezza][];
 			for (int i = 0; i< grandezza; i++)
 			{
 				Disegno[i] = new int[grandezza];
@@ -15,14 +18,14 @@ namespace Tree
 		}
 		public void aggNodo(Alberografico nodo)
 		{
-
+			nodi.Add(nodo);
 		}
 		public void aggLato(int ris, int dest)
 		{
 			Disegno[ris][dest] = 1;
 		}
 		public bool Lato(int ris, int dest)
-		{
+		{ 
 			if(Disegno[ris][dest] == 1)
 			{
 				return true;
@@ -31,15 +34,20 @@ namespace Tree
 		}
 		public void stampa()
 		{
+			foreach(Alberografico n in nodi)
+			{
+				Console.Write(" " + n.Data );
+			}
+			Console.WriteLine("");
 			for (int i = 0; i<Disegno.Length; i++)
 			{
+				Console.Write(nodi[i].Data + "");
                 for (int j = 0; j < Disegno[i].Length; j++)
 				{
-					Console.Write(Disegno[i][j] + "");
+					Console.Write( Disegno[i][j] + " ");
 				}
-				Console.WriteLine("");
+				Console.WriteLine(" ");
             }
 		}
 	}
 }
-
