3298fd1fdc0c9fddd745dbe0556c07baac3f83db
Vtipol <151755385+Vtipol@users.noreply.github.com>
2024-04-11 13:21:37 +0200

    Add files via upload

---
+++ CNT/CNT/BIT.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CNT
+{
+	public class BIT
+	{
+		public Dictionary<string, float> dict = new Dictionary<string, float>();
+
+
+		public void ex(ref Dictionary<string, float> d)
+		{
+            d.Add("2024-01-22", 242);
+            d.Add("2024-01-23", 247);
+            d.Add("2024-01-28", 251);
+			d.Add("2024-01-11", 123);
+            d.Add("2024-02-02", 288);
+            d.Add("2024-02-03", 399);
+            d.Add("2024-02-04", 403);
+			d.Add("2001-12-01", 345);
+            d.Add("2024-02-05", 321);
+            d.Add("2024-02-10", 123);
+
+		}
+		public void CheckK( ref Dictionary<string, float> Dic)
+		{
+			string[] Keys = Dic.Keys.ToArray();
+			int CorrectL;
+			int Dnn = 0;
+			for (int i = 0; i< Keys.Length; i++)
+			{
+				DateTime date = DateTime.Parse(Keys[i]);
+				int year = date.Year;
+				int month = date.Month;
+				int day = date.Day;
+				CorrectL = Keys[i].Length;
+				if(CorrectL != 10)
+				{
+					Dnn++;
+					Dic.Remove(Dic.ElementAt(i).Key);
+					continue;
+				}
+				if (year != 2024)
+				{
+                    Dnn++;
+                    Dic.Remove(Dic.ElementAt(i).Key);
+					continue;
+                }
+			}
+            Console.WriteLine($"date invalide : {Dnn}");
+        }
+		public void Show(Dictionary<string,float> D)
+		{
+			foreach(KeyValuePair<string, float> kvp in D)
+			{
+			Console.WriteLine("Date : {0} Value : {1} ", kvp.Key, kvp.Value);
+			}
+		}
+	}
+}
+
---
+++ CNT/CNT/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CNT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BIT bit = new BIT();
+            Dictionary<string, float> dict = new Dictionary<string, float>();
+            bit.ex(ref dict);
+            bit.CheckK(ref dict);
+            bit.Show(dict);
+
+            Console.ReadKey();
+        }
+    }
+}
